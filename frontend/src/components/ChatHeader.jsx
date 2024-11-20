f04e4d3786636637f748343876e4edfd12db1679
suprince <suprince.shakya@swivt.com.np>
2024-11-20 21:44:21 +0545

    init

---
+++ frontend/src/components/ChatHeader.jsx
@@ -0,0 +1,53 @@
+import { X, Video,PhoneCall,PhoneOutgoing } from "lucide-react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
+
+const ChatHeader = () => {
+  const { selectedUser, setSelectedUser } = useChatStore();
+  const { onlineUsers } = useAuthStore();
+
+  return (
+    <div className="p-2.5 border-b border-base-300">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          {/* Avatar */}
+          <div className="avatar">
+            <div className="size-10 rounded-full relative">
+              <img
+                src={selectedUser.profilePic || "/avatar.png"}
+                alt={selectedUser.fullName}
+              />
+            </div>
+          </div>
+
+          {/* User info */}
+          <div>
+            <h3 className="font-medium">
+              {selectedUser.fullName.charAt(0).toUpperCase() +
+                String(selectedUser.fullName).slice(1)}
+            </h3>
+            <p className="text-sm text-base-content/70">
+              {onlineUsers.includes(selectedUser._id) ? "Online" : "Offline"}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex justify-between">
+          {/* Audio Call button */}
+          <button className="mr-6" onClick={() => setSelectedUser(null)}>
+            <PhoneOutgoing />
+          </button>
+          {/* Video call button */}
+          <button className="mr-6" onClick={() => setSelectedUser(null)}>
+            <Video />
+          </button>
+          {/* Close button */}
+          <button onClick={() => setSelectedUser(null)}>
+            <X />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ChatHeader;
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,85 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+import { useAuthStore } from "./useAuthStore";
+
+export const useChatStore = create((set, get) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      console.log("getUsers", res);
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+      for (const user of get().users) {
+        if (user._id == selectedUser._id) user.recentMessage = res.data;
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+
+  subscribeToMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.on("newMessage", (newMessage) => {
+      console.log(newMessage);
+      const { selectedUser } = get();
+      for (const user of get().users) {
+        if (user._id == newMessage.senderId._id) {
+          user.recentMessage = newMessage;
+          const newUsers = get().users.filter((guser) => user._id != guser._id);
+          set({
+            users: [user, ...newUsers],
+          });
+        }
+      }
+
+      if (!selectedUser) return;
+      const isMessageSentFromSelectedUser =
+        newMessage.senderId._id === (selectedUser._id || false);
+      if (!isMessageSentFromSelectedUser) return;
+
+      set({
+        messages: [...get().messages, newMessage],
+      });
+    });
+  },
+
+  unsubscribeFromMessages: () => {
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage");
+  },
+
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
