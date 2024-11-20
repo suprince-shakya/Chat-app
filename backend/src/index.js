ee2890d2939ef021625354fcb37ac7efe58d7ed3
suprince <suprince.shakya@swivt.com.np>
2024-11-20 22:57:05 +0545

    added .env.example

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,21 +10,17 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+import { corsOption } from "./config/cors.config.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors(corsOption));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
