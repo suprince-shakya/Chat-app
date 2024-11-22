851932de71c2aa8259f8e2ed712bf7b1d514dff2
suprince <suprince.shakya@swivt.com.np>
2024-11-22 14:31:08 +0545

    added exception handlers

--- backend/src/config/express.config.ts
+++ backend/src/config/express.config.ts
@@ -7,6 +7,7 @@ import messageRoutes from '../modules/message/message.route';
 import { connectDB } from './db.config';
 import { app, server } from './socket.config';
 import { corsOption } from './cors.config';
+import errorHandlerUtil from '../core/utils/error-handler.util';
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -25,5 +26,30 @@ if (process.env.NODE_ENV === 'production') {
 		res.sendFile(path.join(__dirname, '../../../frontend', 'dist', 'index.html'));
 	});
 }
+
+const exitHandler = (): void => {
+	if (app) {
+		server.close(() => {
+			console.info('Server closed');
+			process.exit(1);
+		});
+	} else {
+		process.exit(1);
+	}
+};
+
+const unexpectedErrorHandler = (error: Error): void => {
+	errorHandlerUtil.handleError(error);
+	if (!errorHandlerUtil.isTrustedError(error)) {
+		exitHandler();
+	}
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+
+process.on('unhandledRejection', (reason: Error) => {
+	console.error('Unhandled rejection !!!!!!');
+	throw reason;
+});
 
 export default server;
